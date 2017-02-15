2556a74ce43cbe81706b1ff04d2add4cde52d4ac
Philip 1 <Philip 1@DESKTOP-A52IPJD.ColoradoCollege.edu>
2017-02-15 00:43:37 -0700

    jsjs

---
+++ src/TreeNode.java
@@ -0,0 +1,56 @@
+
+
+public class TreeNode<T extends Comparable> {
+
+    private T value;
+    private TreeNode<T> left_child;
+    private TreeNode<T> right_child;
+
+    public TreeNode(T _value) {
+      value = _value;
+      left_child = null;
+      right_child = null;
+    }
+
+    public void addLeftChild(TreeNode<T> _child) {
+      left_child = _child;
+    }
+
+    public void addRightChild(TreeNode<T> _child) {
+      right_child = _child;
+    }
+
+    public TreeNode<T> getLeftChild() {
+      return left_child;
+    }
+
+    public TreeNode<T> getRightChild() {
+      return right_child;
+    }
+
+    public T getValue() {
+      return value;
+    }
+
+    int count() {
+      TreeNode right = getRightChild();
+      TreeNode left = getLeftChild();
+      int c = 1;
+      if ( right != null ) c += right.count();
+      if ( left != null ) c += left.count();
+      return c;
+    }
+
+    public String toString() {
+      return "TreeNode: " + value.toString();
+    }
+
+
+
+
+
+
+
+
+
+}
