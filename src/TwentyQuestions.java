2851fad89bdd6c175a7e81cf25bc67268831de6b
Abe Binder <fxhasthemovies@gmail.com>
2017-02-15 04:36:09 -0700

    questions

--- src/TwentyQuestions.java
+++ src/TwentyQuestions.java
@@ -4,31 +4,88 @@ public class TwentyQuestions {
 	Queue<String> qandAQueue;
 	ArrayList<String>questionsList=new ArrayList<String>();
 	Tree<String> questionTree=new Tree<String>();
+	
+	public void writeArrayListToQuestions(){
+		File file=new File("questions.txt");
+		try{
+		FileWriter fw=new FileWriter(file);
+		BufferedWriter bw=new BufferedWriter(fw);
+		for(int i=0; i<questionsList.size();i++){
+			if(i<questionsList.size()-1){
+			bw.write(questionsList.get(i));
+			bw.newLine();
+			}
+			else{
+				bw.write(questionsList.get(i));
+				bw.close();
+			}
+		}
+		bw.close();
+		
+		} catch(Exception e){System.out.println(e);}
+	}
+	
+	public int getCurrentQuestionFromList(String s){
+		for(int i=0; i<questionsList.size(); i++){
+			if(questionsList.get(i).equals(s)){
+				return i;
+			}
+		}
+		return 0;
+	}
 
+	
 	public void play(){
 		readIn();
 		convertListToTree();
 		Scanner sc=new Scanner(System.in);
 		TreeNode<String>current=questionTree.getRoot();
 		while(true){
-			if(current.getLeftChild()==null&&current.getRightChild()==null){
+			String question=current.getValue();
+		
+			System.out.println(question);
+			String user_input=sc.nextLine();
+			if(user_input.equals("yes")&&current.getLeftChild()!=null){
+				current=current.getLeftChild();
+			}
+			else if(user_input.equals("no")&&current.getRightChild()!=null){
+				current=current.getRightChild();
+			}
+			else if(user_input.equals("no")&&current.left_child==null){
 				System.out.println("enter a new object");
 				String specific_item=sc.nextLine();
 				System.out.println("what distinguishe it");
 				String broad_question=sc.nextLine();
 				System.out.println("does your new object answer yes or no");
 				String answer=sc.nextLine();
-				
-				
-			}
-			String question=current.getValue();
-			System.out.println(question);
-			String user_input=sc.nextLine();
-			if(user_input.equals("yes")){
-				current=current.getLeftChild();
-			}
-			else{
-				current=current.getRightChild();
+				boolean distinguishing_boolean=false;
+				if(answer.equals("yes")){
+					distinguishing_boolean=true;
+				}
+				else{
+					distinguishing_boolean=false;
+				}
+				int index=getCurrentQuestionFromList(question);
+				questionsList.set(index,broad_question);
+				questionsList.add("");
+				questionsList.add("is it a "+specific_item);
+				questionsList.add("");
+				questionsList.add("");
+				questionsList.add(question);
+				questionsList.add("");
+				questionsList.add("");
+				if(distinguishing_boolean){
+					questionsList.set(index+1, questionsList.size()-6+"");
+					questionsList.set(index+2, questionsList.size()-3+"");
+					writeArrayListToQuestions();
+					return;
+				}
+				else{
+					questionsList.set(index+1, questionsList.size()-3+"");
+					questionsList.set(index+2, questionsList.size()-6+"");
+					writeArrayListToQuestions();
+					return;
+				}
 			}
 		}
 	}
